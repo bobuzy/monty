e4dc87cdc9a507e4fe56d52f87a73f53a7ca5339
goody-1 <atundeg@gmail.com>
2024-03-28 23:34:03 +0100

    optimize pchar and pstr

--- complex_opcodes.c
+++ complex_opcodes.c
@@ -8,25 +8,19 @@
  */
 void pchar_func(stack_t **list, unsigned int line_num)
 {
-	stack_t *ptr;
-
 	if (!list || !(*list))
 	{
 		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_num);
 		exit(EXIT_FAILURE);
 	}
 
-	ptr = *list;
-
-	if (ptr->n < 0 || ptr->n > 127)
+	if ((*list)->n < 0 || (*list)->n > 127)
 	{
 		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_num);
-		free_stack(ptr);
 		exit(EXIT_FAILURE);
 	}
 
-	printf("%c\n", ptr->n);
-	free_stack(ptr);
+	printf("%c\n", (*list)->n);
 }
 
 /**
@@ -37,32 +31,15 @@ void pchar_func(stack_t **list, unsigned int line_num)
  */
 void pstr_func(stack_t **list, unsigned int line_num)
 {
-	stack_t *ptr;
+	stack_t *ptr = *list;
 
 	(void)line_num;
 
-	if (!list || !(*list))
+	while (ptr && ptr->n > 0 && ptr->n <= 127)
 	{
-		printf("\n");
-		exit(EXIT_SUCCESS);
+		printf("%c", ptr->n);
+		ptr = ptr->next;
 	}
 
-	ptr = *list;
-
-	while (ptr != NULL)
-	{
-		if (ptr->n < 0 || ptr->n > 127 || ptr->n == 0)
-		{
-			printf("\n");
-			free_stack(ptr);
-			exit(EXIT_SUCCESS);
-		}
-		else
-		{
-			printf("%c", ptr->n);
-			ptr = ptr->next;
-		}
-	}
 	printf("\n");
-	free_stack(ptr);
 }
