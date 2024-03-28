7d675036fac636e8c5bd811f5ba65686d59339e7
goody-1 <atundeg@gmail.com>
2024-03-28 21:39:35 +0100

    separate related functions into modules

--- monty.c
+++ monty.c
@@ -72,62 +72,6 @@ void find_func(char *op_func, char *op_arg, int line_num)
 }
 
 /**
- * push_func - pushes an element to the stack
- * @list: list
- * @value: value to push
- * @line_num: line number
- *
-*/
-void push_func(stack_t **list, int value, unsigned int line_num)
-{
-	stack_t *new = malloc(sizeof(stack_t));
-	(void)line_num;
-
-	if (new == NULL)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
-		exit(EXIT_FAILURE);
-	}
-
-	new->n = value;
-	new->next = *list;
-	new->prev = NULL;
-
-	if (*list != NULL)
-	{
-		(*list)->prev = new;
-	}
-
-	*list = new;
-}
-
-/**
- * pall_func - prints all the values on the stack
- * @list: list
- * @line_num: line number
- *
-*/
-void pall_func(stack_t **list, unsigned int line_num)
-{
-	stack_t *ptr;
-
-	(void)line_num;
-
-	if (list == NULL)
-	{
-		exit(EXIT_FAILURE);
-	}
-
-	ptr = *list;
-
-	while (ptr != NULL)
-	{
-		printf("%d\n", ptr->n);
-		ptr = ptr->next;
-	}
-}
-
-/**
  * free_stack - function that frees a dlistint_t list
  *
  * @head: pointer to head
